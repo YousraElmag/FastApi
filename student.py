f96ddf195a421ead642336bf594f57757a7bd4f0
yousra elmaghraby <yousraelmaghraby@macbook-pro.home>
2025-01-30 22:38:17 +0100

    add cors

--- student.py
+++ student.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 app= FastAPI()
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["*"],
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"]
+)
 class Student(BaseModel):
     id: int
     name: str
